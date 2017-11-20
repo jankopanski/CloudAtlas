94c5a603ec9e88e256ebd4fe20102d9daf3cfddb
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2017-11-21 00:38:40 +0100

    agent CUP

--- agent.sh
+++ agent.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -cp target/classes -Djava.rmi.server.hostname=localhost -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.agent.Main &
+MAVEN=$HOME/.m2/repository
+CUP=$MAVEN/com/github/vbmacher/java-cup-runtime/11b-20160615/java-cup-runtime-11b-20160615.jar
+CLASSPATH=$CUP:target/classes
+
+java -cp $CLASSPATH -Djava.rmi.server.hostname=localhost -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.agent.Main &

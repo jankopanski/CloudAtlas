e4f1cb2b9624d902afabb7c1c7fd0fd1114731bf
Piotr Ciołkosz <pi.ciolkosz@gmail.com>
2017-11-24 19:49:31 +0100

    Forgot to add scripts

---
+++ agentCLI.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp target/classes -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.agent.InterpreterCLI
--- interpreter.sh
+++ interpreter.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -cp target/classes -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.agent.InterpreterCLI
+MAVEN=$HOME/.m2/repository
+CUP=$MAVEN/com/github/vbmacher/java-cup-runtime/11b-20160615/java-cup-runtime-11b-20160615.jar
+CLASSPATH=$CUP:target/classes
+
+java -cp $CLASSPATH pl.edu.mimuw.cloudatlas.interpreter.Main

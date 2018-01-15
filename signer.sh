86fef423fa733859781f19faafc81241e94f942a
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2018-01-15 19:02:46 +0100

    add readme

--- signer.sh
+++ signer.sh
@@ -4,18 +4,4 @@ MAVEN=$HOME/.m2/repository
 CUP=$MAVEN/com/github/vbmacher/java-cup-runtime/11b-20160615/java-cup-runtime-11b-20160615.jar
 CLASSPATH=$CUP:target/classes
 
-if [ $# -eq 2 ]; then
-	RMIHOST=$1
-	RMIPORT=$2
-else
-	RMIPORT=$1
-	if [ -n $HOSTNAME ]; then
-		RMIHOST=$HOSTNAME
-	elif [ -n $HOST ]; then
-		RMIHOST=$HOST
-	else
-		RMIHOST="localhost"
-	fi
-fi
-
-java -cp $CLASSPATH -Djava.rmi.server.hostname=$RMIHOST -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.security.QuerySignerServer query_signer.private $RMIHOST $RMIPORT
+java -cp $CLASSPATH -Djava.rmi.server.hostname=localhost -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.security.QuerySignerServer query_signer.private $1

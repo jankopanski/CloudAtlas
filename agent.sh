bb002a0da7be6b3862dbc952c965356569576101
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2018-01-16 00:55:35 +0100

    agent.sh

--- agent.sh
+++ agent.sh
@@ -5,4 +5,4 @@ CUP=$MAVEN/com/github/vbmacher/java-cup-runtime/11b-20160615/java-cup-runtime-11
 JSON=$MAVEN/com/googlecode/json-simple/json-simple/1.1.1/json-simple-1.1.1.jar
 CLASSPATH=$CUP:$JSON:target/classes
 
-java -cp $CLASSPATH -Djava.rmi.server.hostname=$RMIHOST -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.agent.Main query_signer.public $1
+java -cp $CLASSPATH -Djava.rmi.server.hostname=$1 -Djava.security.policy=common.policy pl.edu.mimuw.cloudatlas.agent.Main "query_signer.public" "$2"
